4994e0249d904681cc60fb342118639d0881450e
曾勇浩 <zengyonghao@outlook.com>
2019-08-08 17:46:12 +0800

    新建login

---
+++ src/components/login/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="login_container">
+       <el_forom ref="loginForm" :model="loginFrom" >
+
+
+
+       </el_forom>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login",
+        data() {
+
+
+        }
+
+
+
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
